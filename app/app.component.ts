610f9bffba5cfb46eca8adb6f1e14f88fcc07cdb
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-22 09:32:24 -0700

    create basic Task with for loop, test tasks

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,28 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+    <div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+     new Task("Eat StringCheese", 0),
+     new Task("Eat Yogurt", 1),
+     new Task("Commit", 2),
+     new Task("Catch Katmandu", 3)
+   ];
+  }
 
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){
+  }
 }
