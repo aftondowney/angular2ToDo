4e4e5728961454d5744aec7730919f6842a479f0
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-03-22 10:58:50 -0700

    add EventEmitter to TaskListComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,33 +1,44 @@
-import { Component } from 'angular2/core';
-
-@Component({
-  selector: 'my-app',
-  template: `
-    <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">{{ task.description }}</h3>
-    <div>
-  `
-})
+import { Component, EventEmitter } from 'angular2/core';
 
 ////TaskListComponent////
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
   template: `
   <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-    {{ currentTask.description }}
+  {{ currentTask.description }}
   </h3>
   `
 })
 
 export class TaskListComponent {
   public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
   taskClicked(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('child', clickedTask);
+    this.onTaskSelect.emit(clickedTask);
   }
 }
 /////end TaskListComponent/////
+
+/////MyAppComponent//////
+@Component({
+  selector: 'my-app',
+  directives: [TaskListComponent],
+  template: `
+    <div class="container">
+      <h1>To-Do List</h1>
+      <task-list [taskList]="tasks"
+      (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
+  `
+})
+
 
 export class AppComponent {
   public tasks: Task[];
